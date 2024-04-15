f72de5afc947d566cf1585f322c0317ac068d519
ghrjs1998 <zbsl4547@naver.com>
2024-04-16 00:26:48 +0900

    온보딩 뷰 구현

---
+++ VoiceMemo/VoiceMemo/VoiceMemo/App/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  VoiceMemo
+//
+//  Created by 박호건 on 4/15/24.
+//
+
+import Foundation
---
+++ VoiceMemo/VoiceMemo/VoiceMemo/App/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by 박호건 on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
