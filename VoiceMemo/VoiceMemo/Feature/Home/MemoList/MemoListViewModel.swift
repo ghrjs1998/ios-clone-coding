5dc28cb7391f373f74f8fbfc53535ef4f063105d
ghrjs1998 <zbsl4547@naver.com>
2024-04-17 00:25:46 +0900

    메모장 화면 구현

--- VoiceMemo/VoiceMemo/Feature/Home/MemoList/MemoListViewModel.swift
+++ VoiceMemo/VoiceMemo/Feature/Home/MemoList/MemoListViewModel.swift
@@ -5,6 +5,80 @@
 
 import Foundation
 
-class MemoListViewModel {
-  
+class MemoListViewModel: ObservableObject {
+    @Published var memos: [Memo]
+    @Published var isEditMemoMode: Bool
+    @Published var removeMemos: [Memo]
+    @Published var isDisplayRemoveMemoAlert: Bool
+    
+    var removeMemoCount: Int {
+        return removeMemos.count
+    }
+    
+    var navigationBarRightBtnMode: NavigationBtnType {
+        isEditMemoMode ? .complete : .edit
+    }
+    
+    init(
+        memos: [Memo] = [],
+        isEditMemoMode: Bool = false,
+        removeMemos: [Memo] = [],
+        isDisplayRemoveMemoAlert: Bool = false
+    ) {
+        self.memos = memos
+        self.isEditMemoMode = isEditMemoMode
+        self.removeMemos = removeMemos
+        self.isDisplayRemoveMemoAlert = isDisplayRemoveMemoAlert
+    }
+}
+
+extension MemoListViewModel {
+    func addMemo(_ memo: Memo) {
+        memos.append(memo)
+    }
+    
+    func updateMemo(_ memo: Memo) {
+        if let index = memos.firstIndex(where: {$0.id == memo.id}) {
+            memos[index] = memo
+        }
+    }
+    
+    func removeMemo(_ memo: Memo) {
+        if let index = memos.firstIndex(where: {$0.id == memo.id}) {
+            memos.remove(at: index)
+        }
+    }
+    
+    func navigationRightBtnTapped() {
+        if isEditMemoMode {
+            if removeMemos.isEmpty {
+                isEditMemoMode = false
+            }else {
+                // 삭제 얼럿 상태값 변경을 위한 메서드 호출
+                setIsDisplayRemovememoAlert(true)
+            }
+        } else {
+            isEditMemoMode = true
+        }
+    }
+    
+    func setIsDisplayRemovememoAlert(_ isDisplay: Bool) {
+        isDisplayRemoveMemoAlert = isDisplay
+    }
+    
+    func memoRemoveSelectedBoxTapped(_ memo: Memo) {
+        if let index = removeMemos.firstIndex(of: memo) {
+            removeMemos.remove(at: index)
+        } else {
+            removeMemos.append(memo)
+        }
+    }
+    
+    func removeBtnTapped() {
+        memos.removeAll { memo in
+            removeMemos.contains(memo)
+        }
+        removeMemos.removeAll()
+        isEditMemoMode = false
+    }
 }
